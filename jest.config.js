0de54022e5c51c44fff3c05b552e742ccd950a50
Ahn <anhpnnd@gmail.com>
2020-05-17 12:12:01 +0200

    chore: setup path mappings

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: [
+    '<rootDir>/setup-jest.ts',
+  ],
+  testMatch: [
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: [
+        require.resolve('jest-preset-angular/build/InlineFilesTransformer'),
+        require.resolve('jest-preset-angular/build/StripStylesTransformer'),
+      ],
+    },
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  coverageReporters: [
+    'json',
+    'html',
+    'text',
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
+};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,31 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SharedModule } from '@shared/shared.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        SharedModule,
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'jest-angular'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('jest-angular');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
