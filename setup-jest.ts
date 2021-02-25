68f24ded5e395b54ac58f394c79425902afc18ef
Ahn <anhpnnd@gmail.com>
2021-02-25 23:27:42 +0100

    upgrade deps

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,15 +1,1 @@
-import 'jest-preset-angular/setup-jest';
-import './jest-global.mocks';
-
-/**
- * ISSUE: https://github.com/angular/material2/issues/7101
- * Workaround for JSDOM missing transform property
- */
-Object.defineProperty(document.body.style, 'transform', {
-  value: () => {
-    return {
-      enumerable: true,
-      configurable: true,
-    };
-  },
-});
+import 'jest-preset-angular';
