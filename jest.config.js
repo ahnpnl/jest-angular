3ab820e2c3a53b5e6552a824ebcc669516da14c7
Ahn <anhpnnd@gmail.com>
2021-02-25 23:29:57 +0100

    upgrade deps

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig.json');
 
-/** @typedef {import('ts-jest')} */
-/** @type {import('@jest/types').Config.InitialOptions} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  coverageReporters: [
-    'json',
-    'html',
-    'text',
-  ],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
